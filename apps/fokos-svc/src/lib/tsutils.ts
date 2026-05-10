ca559fc5d1c332cedaedf80a1d3c6f17785f1c0e
Lambros Petrou <lambrospetrou@gmail.com>
2026-05-10 18:00:06 +0100

    fix

--- apps/fokos-svc/src/lib/tsutils.ts
+++ apps/fokos-svc/src/lib/tsutils.ts
@@ -1,3 +1,3 @@
 export function assertExists<T>(val: T | undefined | null): asserts val is T {
-	if (val === Array || val === null) throw new Error("Value is missing");
+	if (val === undefined || val === null) throw new Error("Value is missing");
 }
