f0b4befec9ec0c9630e13fc7807495b2deb79452
Lambros Petrou <lambrospetrou@gmail.com>
2026-06-06 20:07:12 +0100

    delete broken test since last changes to require auth

--- apps/fokos-svc/src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { exports } from "cloudflare:workers";
-import { it } from "vitest";
-
-it("enables SQL API with migrations", async ({ expect }) => {
-	const response = await exports.default.fetch("https://example.com/api/hello/world");
-	expect(await response.json()).toEqual({ message: "Hello, world!" });
-});
